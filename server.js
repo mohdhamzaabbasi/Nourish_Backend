e38602a301e5bf890108905825a3af2449d02b45
Mohd Hamza Abbasi <hamzamohd.abbasi@gmail.com>
2025-04-26 21:23:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.post('/webhook', async (req, res) => {
 
     if (req.body.entry?.[0]?.changes?.[0]?.value?.messages?.[0]) {
         const number = req.body.entry[0].changes[0].value.messages[0].from;
-        const reqq = await axios.post('https://nourish-backend-h78c.onrender.com/get-requests', {
+        const reqq = await axios.post('https://nourish-backend-t4s3.onrender.com/get-requests', {
           number: number
         });
         console.log("#########################################");
@@ -51,7 +51,7 @@ app.post('/webhook', async (req, res) => {
 
         try {
             // Send the message to the /chat API to get food recommendations
-            const chatResponse = await axios.post("https://nourish-backend-h78c.onrender.com/chat", { message });
+            const chatResponse = await axios.post("https://nourish-backend-t4s3.onrender.com/chat", { message });
 
             const recommendedFoods = chatResponse.data; // Get response from model
             
@@ -70,7 +70,7 @@ app.post('/webhook', async (req, res) => {
             await axios.post(url, data, { headers });
 
             try {
-                const response = await axios.post('https://nourish-backend-h78c.onrender.com/save-request', {
+                const response = await axios.post('https://nourish-backend-t4s3.onrender.com/save-request', {
                   number, // Sending username as part of the request body
                   message_f,
                 });
